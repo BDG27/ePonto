29bc31adc3a9c4b739e4c3c6dfaeb4d30a4b1c5f
unknown <joaogabriel1412@gmail.com>
2022-08-06 15:26:26 -0300

    Eslint e prettier added

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -6,9 +6,8 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(routes)
-app.use(errorsHandler)
+app.use(express.json());
+app.use(routes);
+app.use(errorsHandler);
 
 export { app };
-
