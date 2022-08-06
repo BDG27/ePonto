5fa3ad64cb509137b7c02bce9e1da7392d500bec
BDG27 <brunodemarchi30@gmail.com>
2022-08-06 15:26:42 -0300

    criação do model user

---
+++ src/modules/user/typeorm/entities/User.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column({ unique: true })
+    email: string
+
+    @Column()
+    password: string
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date
+
+    @DeleteDateColumn({ name: 'deletedAt' })
+    deletedAt: Date
+}
